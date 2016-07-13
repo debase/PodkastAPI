4e778ff49411e9347909928a12f98ea854b1dd39
Etienne DEBAS <etienne.debas@epitech.eu>
2016-07-13 14:18:45 -0700

    change server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var app         = express();
 
 var auth = require('./app/auth/auth');
 
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
